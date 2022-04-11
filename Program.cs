9590d0746909d6a2941b72d9b9bd4496281740b0
viviam_ribeiro <Viviam.Ribeiro@haw-hamburg.de>
2022-04-11 11:29:24 +0200

    Main-Klasse erstellt

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace jazg;
+/// <summary>
+/// Class that sets up model, configures the scenerario and starts the simulation
+/// </summary>
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
