ace97ad0e2eda16f93bfa34ecedb1ab2bc062eee
viviam_ribeiro <Viviam.Ribeiro@haw-hamburg.de>
2022-04-11 11:25:28 +0200

    added abstract agent class

---
+++ Model/Players/Player.cs
@@ -0,0 +1,23 @@
+﻿using Mars.Components.Agents;
+
+namespace jazg.Model.Players;
+
+/// <summary>
+/// abstract class to define player properties and main behaviour.
+/// Zombies and humans inherit from this class and can specify their behaviour in their concrete classes
+/// </summary>
+public abstract class Player : IAgent<PlayerLayer>
+{
+    public void Init(MyLayer layer) 
+    {
+        // finalize the init process
+    }
+
+    public void Tick() 
+    {
+        // define your logic here
+    }
+
+    public Guid ID { get; set; }
+}
+}
