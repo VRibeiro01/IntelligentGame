02f7ff982222f92017f691c86768c4aa2d36a169
viviam_ribeiro <Viviam.Ribeiro@haw-hamburg.de>
2022-04-18 18:27:07 +0200

    added wall class

--- JAZG/JAZG/Model/Objects/Item.cs
+++ JAZG/JAZG/Model/Objects/Item.cs
@@ -1,25 +1,21 @@
-﻿using System;
+using System;
 using Mars.Components.Environments.Cartesian;
+using NetTopologySuite.Geometries;
 
 namespace JAZG.Model.Objects
 {
     public abstract class Item : IObstacle
     {
         public Guid ID { get; set; }
+       
 
-        public bool IsRoutable(ICharacter character)
-        {
-            throw new NotImplementedException();
-        }
+        public abstract bool IsRoutable(ICharacter character);
 
-        public CollisionKind? HandleCollision(ICharacter character)
-        {
-            throw new NotImplementedException();
-        }
 
-        public VisibilityKind? HandleExploration(ICharacter explorer)
-        {
-            throw new NotImplementedException();
-        }
+        public abstract CollisionKind? HandleCollision(ICharacter character);
+
+
+        public abstract VisibilityKind? HandleExploration(ICharacter explorer);
+
     }
 }
---
+++ JAZG/JAZG/Model/Objects/Wall.cs
@@ -0,0 +1,23 @@
+﻿using Mars.Components.Environments.Cartesian;
+
+namespace JAZG.Model.Objects
+{
+    public class Wall : Item
+    {
+        public override bool IsRoutable(ICharacter character)
+        {
+            // this means that the wall cannot be passed (??? right ??)
+            return false;
+        }
+
+        public override CollisionKind? HandleCollision(ICharacter character)
+        {
+            return CollisionKind.Block;
+        }
+
+        public override VisibilityKind? HandleExploration(ICharacter explorer)
+        {
+            return VisibilityKind.Opaque;
+        }
+    }
+}
