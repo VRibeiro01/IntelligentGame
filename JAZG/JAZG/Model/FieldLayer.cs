ca5437c871142790dc87ff851e4814754445f30b
viviam_ribeiro <Viviam.Ribeiro@haw-hamburg.de>
2022-04-29 15:35:18 +0200

    Bounded spatial extent of enviroment to layer i

--- JAZG/JAZG/Model/FieldLayer.cs
+++ JAZG/JAZG/Model/FieldLayer.cs
@@ -9,6 +9,7 @@ using Mars.Core.Data;
 using Mars.Interfaces.Data;
 using Mars.Interfaces.Environments;
 using Mars.Interfaces.Layers;
+using MongoDB.Driver.Core.Events;
 using NetTopologySuite.Geometries;
 using Position = Mars.Interfaces.Environments.Position;
 
@@ -32,10 +33,11 @@ namespace JAZG.Model
             if (inputs != null && inputs.Any())
             {
                 Environment = new CollisionEnvironment<Player, Item>();
-                
+              
+
                 // TODO: improve this
                 Environment.BoundingBox =
-                    new BoundingBox(new Position(0, 0), new Position(100, 100));
+                    new BoundingBox(new Position(0, 0), new Position(Width-1, Height-1));
                 Coordinate[] wall1 = {new(0, 0), new(0, 100)};
                 Coordinate[] wall2 = {new(0, 0), new(100, 0)};
                 Coordinate[] wall3 = {new(100, 100), new(0, 100)};
@@ -58,6 +60,8 @@ namespace JAZG.Model
             //TODO create walls and place them on the field
             // TODO create food class and place them on field + functionality when human finds food
             // TODO create Weapon Gun 
+            
+           
             return true;
         }
 
