53e73dfe218b6e1c6f2cd4c91f46c5fbb619995b
viviam_ribeiro <Viviam.Ribeiro@haw-hamburg.de>
2022-05-16 12:43:16 +0200

    QHumanLearning

---
+++ JAZG/JAZG/Model/Learning/QHumanLearning.cs
@@ -0,0 +1,33 @@
+﻿using Mars.Components.Services.Explorations;
+using Mars.Components.Services.Learning;
+using Mars.Interfaces.Environments;
+using Mars.Numerics;
+
+namespace JAZG.Model.Learning
+{
+    public class QHumanLearning
+    {
+        // possible distances from zombie
+        public int states;
+
+        // amount of possible actions: run or shoot
+        public int actions;
+
+        //implements method to choose next action according to Roulette Wheel 
+        public IExplorationPolicy ExplorationPolicy;
+
+        // QLearning class contains table with states and a zero-initialized double array where length=actions
+        public static QLearning QLearning { get; set; }
+
+        public QHumanLearning(int maxSeeingDistance)
+        {
+            states = maxSeeingDistance;
+            actions = 2;
+            ExplorationPolicy = new RouletteWheelExploration();
+            QLearning = new QLearning(states, actions, ExplorationPolicy);
+
+        }
+
+
+    }
+}
